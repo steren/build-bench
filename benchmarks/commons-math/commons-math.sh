1493e9d1b5574f38e93d9f194c9ddf668d64551e
Thibault Kruse <thibault.kruse@gmx.de>
2016-09-25 16:25:28 +0900

    scripts to run full benchmarks

---
+++ benchmarks/commons-math/commons-math.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env sh
+rm -rf ../../build/commons-math
+rm -f ../../build/reports/commons-math.csv
+rm ./*.csv
+rm -rf ../../caches/bazel/bazel0.1.4rc2/bazel_cache
+
+make -C ../.. CONFIG=configs/commons-math.mk
+mv ../../build/reports/commons-math.csv ./sample1.csv
+
+make -C ../.. CONFIG=configs/commons-math.mk
+mv ../../build/reports/commons-math.csv ./sample2.csv
+
+cp ../../build/commons-math/source/src/main/java/org/apache/commons/math3/linear/AbstractFieldMatrix.java .
+cp AbstractFieldMatrix.java.modified ../../build/commons-math/source/src/main/java/org/apache/commons/math3/linear/AbstractFieldMatrix.java
+
+make -C ../.. CONFIG=configs/commons-math.mk
+mv ../../build/reports/commons-math.csv ./sample3.csv
+
+cp -f AbstractFieldMatrix.java ../../build/commons-math/source/src/main/java/org/apache/commons/math3/linear/AbstractFieldMatrix.java
+
+make -C ../.. CONFIG=configs/commons-math.mk
+mv ../../build/reports/commons-math.csv ./sample4.csv
---
+++ benchmarks/multi/multi.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env sh
+rm -rf ../../build/multi-16-10
+rm -f ../../build/reports/multi-16-10.csv
+rm ./*.csv
+rm -rf ../../caches/bazel/bazel0.14.rc2/bazel_cache
+
+make -C ../.. CONFIG=configs/generated_multi.mk
+mv ../../build/reports/multi-16-10.csv ./sample1.csv
+
+make -C ../.. CONFIG=configs/generated_multi.mk
+mv ../../build/reports/multi-16-10.csv ./sample2.csv
+
+cp ../../build/multi-16-10/source/subproject0/src/main/java/com0/Simple0.java  .
+cp Simple0.java.modified ../../build/multi-16-10/source/subproject0/src/main/java/com0/Simple0.java
+touch ../../build/multi-16-10/source
+
+make -C ../.. CONFIG=configs/generated_multi.mk
+mv ../../build/reports/multi-16-10.csv ./sample3.csv
+
+cp -f Simple0.java ../../build/multi-16-10/source/subproject0/src/main/java/com0/Simple0.java
+touch ../../build/multi-16-10/source
+
+make -C ../.. CONFIG=configs/generated_multi.mk
+mv ../../build/reports/multi-16-10.csv ./sample4.csv
